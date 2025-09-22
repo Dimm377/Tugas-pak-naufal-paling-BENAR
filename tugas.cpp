d8a8acbb902519f56c457068d0e023b44213a729
Dimm377 <dimaswijay2345@gmail.com>
2025-09-22 10:14:32 +0700

    Tugas cpp

---
+++ tugas.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+      cout << "Nama : Dimas Putra wijaya" << endl;
+      cout << "NIM : A11.2025.16431" << endl;
+      int a = 15, b = 5;
+      cout << "input a = " << a << endl;
+      cout << "input b = " << b << endl;
+
+      cout << "input a + b = " << a + b << endl;
+      cout << "input a - b = " << a - b << endl;
+      cout << "input a * b = " << a * b << endl;
+      cout << "input a / b = " << a / b << endl;
+      return 0;
+}
